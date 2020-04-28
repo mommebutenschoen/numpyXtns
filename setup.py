715c9992e1b8a7346d7220853ae344464f733dc8
Momme Butenschön <momme.butenschon@cmcc.it>
2020-04-28 20:26:53 +0200

    Release 20.04.1.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='20.04',
+    version='20.04.1',
 
     description='Numpy extension functions.',
     long_description=long_description,
